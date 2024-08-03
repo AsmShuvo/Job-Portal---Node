14ccc427a5b1cfd1b6b03a86307116718eb87e02
AsmShuvo <hoffmanhulk99@gmail.com>
2024-08-03 22:42:34 +0600

    add: update jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -24,4 +24,26 @@ const getJobs = async (req, res, next) => {
   } catch (error) {}
 };
 
-module.exports = { createJob, getJobs };
+const updateJobs = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  // validation
+  // if (!company || !position) {
+  //   next("Please provide all fields");
+  // }
+  // find job
+  const job = await jobsModel.findOne({ _id: id });
+  if (!job) {
+    next("No jobs found with this id");
+  }
+  if (! req.user.userId === job.createdBy.toString()) {
+    return next("You don't have permission to update this job");
+  }
+  const updatedJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ updatedJob });
+};
+
+module.exports = { createJob, getJobs, updateJobs };
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const userAuth = require("../middlewares/authMiddleware");
-const { createJob, getJobs } = require("../controllers/jobsController");
+const {
+  createJob,
+  getJobs,
+  updateJobs,
+} = require("../controllers/jobsController");
 const router = express.Router();
 
-// routes
 // create job
 router.post("/create-job", userAuth, createJob);
 // get jobs
 router.get("/get-job", userAuth, getJobs);
-
+// update jobs
+router.patch("/update-job/:id", userAuth, updateJobs);
 module.exports = router;
